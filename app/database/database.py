2191e1ac482ffcf6a3ee3d01106d5db959f44f78
Vedant Chaudhari <vedant.chaudhari.777@gmail.com>
2023-02-01 12:24:58 +0530

    Update database.py

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:vedant@localhost:5432/taskdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://[DB_USER]:[DB_PASSWORD]@[DB_HOST]:[DB_PORT]/[DB_NAME]"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
